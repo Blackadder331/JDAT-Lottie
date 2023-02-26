71b37a000a0329063a3db8f75ac0f0e77c6aeafe
R Sharp <reynolds.sharp@gmail.com>
2023-02-26 16:36:40 -0600

    working Lottie file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'JDAT_Logo_Animate_FNL.json'
+})
